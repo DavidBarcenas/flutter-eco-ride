79ec3bb7378d91745e99fd9b24a6c8ee605ced1c
David Barcenas <daveepro@outlook.com>
2022-06-23 12:39:28 -0500

    add dotenv package and change min sdk version

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Text(dotenv.env['FOOl'] ?? 'Prueba'),
       ),
     );
   }
