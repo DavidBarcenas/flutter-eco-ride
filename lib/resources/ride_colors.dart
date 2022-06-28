2f7bcf9a210df904eccda27d60ba0b29947693c4
David Barcenas <daveepro@outlook.com>
2022-06-27 20:43:45 -0500

    add ride colors

---
+++ lib/resources/ride_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RideColors {
+  static const Color primary = Color(0xFF2B1A64);
+  static const Color primaryDark = Color(0xFF1c3aa9);
+  static const Color accent = Color(0xFF21ba45);
+  static const Color accent1 = Color(0x00ffe3fd);
+  static const Color background = Color(0x00fffbfa);
+  static const Color pink = Color(0xFFE66C75);
+  static const Color orange = Color(0xFFE8913A);
+  static const Color blue = Color(0xFF2254A3);
+  static const Color accentPurple = Color(0x00ff4f5c);
+  static const Color text = Color(0xFF383635);
+  static const Color textLight = Color(0xFF918D8D);
+  static const Color textSemiLight = Color(0xFF737373);
+  static const Color textDark = Color(0x00ff2928);
+  static const Color green = Color(0xFF40cf89);
+  static const Color lightGray = Color(0xFFe2e2e2);
+  static const Color lightGrayFair = Color(0xFFe1e5e8);
+  static const Color dimText = Color(0xFFadadad);
+}
