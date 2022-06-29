73281079d77207179f5b2cad3a760681fa541902
David Barcenas <daveepro@outlook.com>
2022-06-29 15:16:00 -0500

    add login functionallity

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,83 +1,160 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:ecoride/resources/ride_colors.dart';
 import 'package:ecoride/resources/strings.dart';
+import 'package:ecoride/screens/home_page.dart';
 import 'package:ecoride/screens/register_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../widgets/button.dart';
 import '../widgets/logo.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   static const String id = 'login';
-
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey =
+      GlobalKey<ScaffoldMessengerState>();
+
+  void validateFields() {
+    if (!emailController.text.contains('@')) {
+      showSnackBar('Ingresa un correo electrónico válido');
+      return;
+    }
+    if (passwordController.text.length < 8) {
+      showSnackBar('La contraseña debe tener al menos 8 caracteres');
+      return;
+    }
+    login();
+  }
+
+  void login() async {
+    final User? user = (await _auth
+            .signInWithEmailAndPassword(
+                email: emailController.text, password: passwordController.text)
+            .catchError((exception) {
+      FirebaseAuthException thisEx = exception;
+      showSnackBar(thisEx.message ?? 'Algo salió mal. Intentalo más tarde');
+    }))
+        .user;
+
+    if (user != null) {
+      DatabaseReference userRef =
+          FirebaseDatabase.instance.ref('users/${user.uid}');
+
+      userRef.once().then((resp) => {
+            if (resp.snapshot.value != null)
+              {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, HomePage.id, (route) => false)
+              }
+          });
+    }
+  }
+
+  void checkConnectivity() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult != ConnectivityResult.mobile &&
+        connectivityResult != ConnectivityResult.wifi) {
+      showSnackBar('No tienes conexión a internet');
+      return;
+    }
+  }
+
+  void showSnackBar(String title) {
+    final snackbar = SnackBar(
+        content: Text(
+      title,
+      textAlign: TextAlign.center,
+      style: const TextStyle(fontSize: 15),
+    ));
+    _scaffoldKey.currentState?.showSnackBar(snackbar);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  const SizedBox(height: 70),
-                  const Logo(),
-                  const SizedBox(height: 40),
-                  const Text(Strings.signIn,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontFamily: 'Brand-Bold', fontSize: 25)),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        const TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                              labelText: Strings.emailAddress,
-                              labelStyle: TextStyle(fontSize: 14.0),
-                              hintStyle: TextStyle(
-                                  color: Colors.grey, fontSize: 10.0)),
-                          style: TextStyle(fontSize: 14),
-                        ),
-                        const SizedBox(height: 10),
-                        const TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                labelText: Strings.password,
+    return ScaffoldMessenger(
+      key: _scaffoldKey,
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 70),
+                    const Logo(),
+                    const SizedBox(height: 40),
+                    const Text(Strings.signIn,
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontFamily: 'Brand-Bold', fontSize: 25)),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                                labelText: Strings.emailAddress,
                                 labelStyle: TextStyle(fontSize: 14.0),
                                 hintStyle: TextStyle(
                                     color: Colors.grey, fontSize: 10.0)),
-                            style: TextStyle(fontSize: 14)),
-                        const SizedBox(height: 40),
-                        Button(
-                          title: Strings.enterBtn,
-                          color: RideColors.green,
-                          onPressed: () {},
-                        ),
-                        const SizedBox(height: 15),
-                      ],
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                          const SizedBox(height: 10),
+                          TextField(
+                              controller: passwordController,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                  labelText: Strings.password,
+                                  labelStyle: TextStyle(fontSize: 14.0),
+                                  hintStyle: TextStyle(
+                                      color: Colors.grey, fontSize: 10.0)),
+                              style: const TextStyle(fontSize: 14)),
+                          const SizedBox(height: 40),
+                          Button(
+                            title: Strings.enterBtn,
+                            color: RideColors.green,
+                            onPressed: () => validateFields(),
+                          ),
+                          const SizedBox(height: 15),
+                        ],
+                      ),
                     ),
-                  ),
-                  Center(
-                    child: RichText(
-                        text: TextSpan(children: [
-                      const TextSpan(
-                          text: "${Strings.dontHaveAccount} ",
-                          style: TextStyle(color: RideColors.textLight)),
-                      TextSpan(
-                          text: Strings.signUp,
-                          style: const TextStyle(color: RideColors.green),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () => Navigator.pushNamedAndRemoveUntil(
-                                context, RegisterPage.id, (route) => false)),
-                    ])),
-                  )
-                ],
+                    Center(
+                      child: RichText(
+                          text: TextSpan(children: [
+                        const TextSpan(
+                            text: "${Strings.dontHaveAccount} ",
+                            style: TextStyle(color: RideColors.textLight)),
+                        TextSpan(
+                            text: Strings.signUp,
+                            style: const TextStyle(color: RideColors.green),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () => Navigator.pushNamedAndRemoveUntil(
+                                  context, RegisterPage.id, (route) => false)),
+                      ])),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ));
+          )),
+    );
   }
 }
