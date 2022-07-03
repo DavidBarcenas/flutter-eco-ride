fb5376ed04587110558918cbd2415c2310233335
David Barcenas <daveepro@outlook.com>
2022-07-02 19:22:30 -0500

    fetch user's current location

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:ecoride/resources/ride_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:line_icons/line_icons.dart';
 import 'dart:async';
@@ -22,11 +23,21 @@ class _HomePageState extends State<HomePage> {
   late GoogleMapController mapController;
   double mapBottomPadding = 0;
   double searchPanelHeight = (Platform.isIOS) ? 300 : 275;
+  late Position userPosition;
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  void setupPositionLocator() async {
+    Position position = await _determinePosition();
+    userPosition = position;
+
+    LatLng pos = LatLng(position.latitude, position.longitude);
+    CameraPosition cp = CameraPosition(target: pos, zoom: 14.0);
+    mapController.animateCamera(CameraUpdate.newCameraPosition(cp));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -126,6 +137,9 @@ class _HomePageState extends State<HomePage> {
                 mapType: MapType.normal,
                 myLocationButtonEnabled: true,
                 initialCameraPosition: _kGooglePlex,
+                myLocationEnabled: true,
+                zoomControlsEnabled: true,
+                zoomGesturesEnabled: true,
                 onMapCreated: (GoogleMapController controller) {
                   _controller.complete(controller);
                   mapController = controller;
@@ -133,6 +147,7 @@ class _HomePageState extends State<HomePage> {
                   setState(() {
                     mapBottomPadding = (Platform.isIOS) ? 270 : 280;
                   });
+                  setupPositionLocator();
                 }),
             Positioned(
               top: 44,
@@ -276,5 +291,41 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ));
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
   }
 }
