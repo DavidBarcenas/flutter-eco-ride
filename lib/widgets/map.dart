75a2f18e2ead0d6fc9412e502700b584afe77031
David Barcenas <daveepro@outlook.com>
2022-07-18 23:02:34 -0500

    update helper methods

--- lib/widgets/map.dart
+++ lib/widgets/map.dart
@@ -69,7 +69,7 @@ class _GMapState extends State<GMap> {
   }
 
   void setupPositionLocator() async {
-    Position currentPosition = await _determinePosition();
+    Position currentPosition = await HelperMethods.determinePosition();
     LatLng position = LatLng(currentPosition.latitude, currentPosition.longitude);
     CameraPosition cp = CameraPosition(target: position, zoom: 14.0);
     mapController.animateCamera(CameraUpdate.newCameraPosition(cp));
@@ -79,41 +79,5 @@ class _GMapState extends State<GMap> {
       if (!mounted) return;
       Provider.of<AppData>(context, listen: false).updatePickupAddress(address);
     }
-  }
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error(Strings.locationDisabled);
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error(Strings.locationPermissionDenied);
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(Strings.locationCannotRequest);
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
   }
 }
