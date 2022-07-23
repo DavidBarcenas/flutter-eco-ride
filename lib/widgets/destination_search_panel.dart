f37b0beb35fe981d2088f85f2cd9d423b1c69718
David Barcenas <daveepro@outlook.com>
2022-07-22 19:47:03 -0500

    fix bottom panel positioned

--- lib/widgets/destination_search_panel.dart
+++ lib/widgets/destination_search_panel.dart
@@ -44,86 +44,88 @@ class _DestinationSearchPanelState extends State<DestinationSearchPanel> {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: 0,
-      right: 0,
-      bottom: 0,
-      child: Container(
-        height: widget.panelHeight,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(15.0), topRight: Radius.circular(15.0)),
-            boxShadow: [
-              BoxShadow(color: Colors.black26, blurRadius: 15.0, spreadRadius: 0.5, offset: Offset(0.7, 0.7))
-            ]),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 5,
-              ),
-              const Text(
-                Strings.panelTitle,
-                style: TextStyle(fontSize: 10),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Text(
-                Strings.panelSearchLabel,
-                style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              GestureDetector(
-                onTap: () async {
-                  var response =
-                      await Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage()));
-                  if (response == 'getDirection') {
-                    await getDirection();
-                    widget.showPanel();
-                  }
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(4.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Row(children: const [
-                      Icon(
-                        Icons.search,
-                        color: Colors.blueAccent,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(Strings.panelInputSearchPlaceholder)
-                    ]),
+    return Stack(children: [
+      Positioned(
+        left: 0,
+        right: 0,
+        bottom: 0,
+        child: Container(
+          height: widget.panelHeight,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(15.0), topRight: Radius.circular(15.0)),
+              boxShadow: [
+                BoxShadow(color: Colors.black26, blurRadius: 15.0, spreadRadius: 0.5, offset: Offset(0.7, 0.7))
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  Strings.panelTitle,
+                  style: TextStyle(fontSize: 10),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  Strings.panelSearchLabel,
+                  style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    var response =
+                        await Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage()));
+                    if (response == 'getDirection') {
+                      await getDirection();
+                      widget.showPanel();
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(children: const [
+                        Icon(
+                          Icons.search,
+                          color: Colors.blueAccent,
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(Strings.panelInputSearchPlaceholder)
+                      ]),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 22.0,
-              ),
-              _panelItem(LineIcons.home, Strings.panelHome, Strings.panelHomeDefault),
-              const SizedBox(
-                height: 10.0,
-              ),
-              const CustomDivider(),
-              const SizedBox(
-                height: 16.0,
-              ),
-              _panelItem(LineIcons.building, Strings.panelWork, Strings.panelWorkDefault)
-            ],
+                const SizedBox(
+                  height: 22.0,
+                ),
+                _panelItem(LineIcons.home, Strings.panelHome, Strings.panelHomeDefault),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const CustomDivider(),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                _panelItem(LineIcons.building, Strings.panelWork, Strings.panelWorkDefault)
+              ],
+            ),
           ),
         ),
       ),
-    );
+    ]);
   }
 
   Widget _panelItem(IconData icon, String title, String address) {
